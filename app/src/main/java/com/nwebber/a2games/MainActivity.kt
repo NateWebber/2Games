a93ab035b3400ca31efa4952e4faa5de4909280f
NateWebber <nwebber@u.rochester.edu>
2021-03-14 14:31:38 -0400

    colors and textview shows games played

--- app/src/main/java/com/nwebber/a2games/MainActivity.kt
+++ app/src/main/java/com/nwebber/a2games/MainActivity.kt
@@ -2,10 +2,19 @@ package com.nwebber.a2games
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var gamesCounter = 0
+    private lateinit var gamesCounterNumber: TextView
+    private lateinit var startCardsButton: Button
+    private lateinit var startTTTButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        gamesCounter = 0
+        gamesCounterNumber.setText(gamesCounter)
     }
 }
