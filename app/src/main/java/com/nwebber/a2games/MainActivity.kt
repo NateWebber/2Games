b8bec569104850c019b1a5f87b595a3cc5702e1d
NateWebber <nwebber@u.rochester.edu>
2021-03-14 15:16:06 -0400

    began ttt fragment

--- app/src/main/java/com/nwebber/a2games/MainActivity.kt
+++ app/src/main/java/com/nwebber/a2games/MainActivity.kt
@@ -15,14 +15,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        startCardsButton = findViewById(R.id.start_card_button)
+        startTTTButton = findViewById(R.id.start_ttt_button)
         gamesCounter = 0
         gamesCounterNumber = findViewById(R.id.times_played_counter)
         gamesCounterNumber.setText(gamesCounter.toString())
 
         if (savedInstanceState == null){
             supportFragmentManager.beginTransaction().replace(R.id.frameLayout, CardsFragment.newInstance()).commitNow()
+            startCardsButton.isEnabled = false
+            startTTTButton.isEnabled = true
         }
 
+        startCardsButton.setOnClickListener {
+            incrementCounter()
+            startCardsButton.isEnabled = false
+            startTTTButton.isEnabled = true
+            supportFragmentManager.beginTransaction().replace(R.id.frameLayout, CardsFragment.newInstance()).commitNow()
+        }
+        startTTTButton.setOnClickListener {
+            incrementCounter()
+            startCardsButton.isEnabled = true
+            startTTTButton.isEnabled = false
+            supportFragmentManager.beginTransaction().replace(R.id.frameLayout, TicTacToeFragment.newInstance()).commitNow()
+        }
+    }
+
+    //quickly add one to game counter and update the text view
+    private fun incrementCounter(){
+        gamesCounter++
+        gamesCounterNumber.setText(gamesCounter.toString())
+    }
+    enum class current_game{
+        CARDS, TTT;
     }
 }
